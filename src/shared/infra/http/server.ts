32e420fb77022d888b9965f11318b0e116ddd92f
Didier <didierpg@gmail.com>
2020-11-07 17:26:36 -0300

    Arquitetura e testes no Node.js ▸ Arquitetura e DDD ▸ Camada de Infra

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,11 +5,11 @@ import cors from 'cors';
 
 import 'express-async-errors';
 
-import routes from './shared/routes';
-import uploadConfig from './config/upload';
+import routes from './routes';
+import uploadConfig from '../../../config/upload';
 import AppError from './errors/AppError';
 
-import './shared/database';
+import '../typeorm';
 
 const app = express();
 
