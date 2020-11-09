5d5b013fa8390f6c2a043f8ceead88c613c598e2
Didier <didierpg@gmail.com>
2020-11-08 21:13:39 -0300

    Arquitetura e testes no Node.js ▸ Arquitetura e DDD ▸ Liskov Substitution Principle

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,15 +1,18 @@
 import { EntityRepository, Repository } from 'typeorm';
 
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
 import Appointment from '../entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date },
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
