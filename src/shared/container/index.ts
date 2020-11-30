9cbf54a9e2e873f4186735deb223ac01bc8c49b1
Didier <didierpg@gmail.com>
2020-11-29 21:15:49 -0300

    Arquitetura e testes no Node.js ▸ Testes e TDD ▸ Testando autenticação

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,10 @@
 import { container } from 'tsyringe';
 
+import '@modules/users/providers';
+
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
