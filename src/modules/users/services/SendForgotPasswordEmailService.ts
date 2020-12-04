c451fb3a20bf00d412a97dbb0a2c173dac844a68
Didier <didierpg@gmail.com>
2020-12-03 22:26:08 -0300

    Continuando back-end do app ▸ Perfil do usuário ▸ Aplicando TDD na prática

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,30 @@
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+import IUserRepository from '../repositories/IUsersRepository';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
