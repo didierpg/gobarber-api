6bca0475e9bee774238bea9558735b969ab90ebd
Didier <didierpg@gmail.com>
2021-01-25 20:36:30 -0300

    Finalizando back-end do app ▸ Cache e segurança ▸ Configurando cache
    docker run --name redis -p 6379:6379 -d -t redis:alpine
    yarn add ioredis
    yarn add -D @types/ioredis

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -14,6 +15,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
